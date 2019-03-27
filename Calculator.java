73a2b646bc143a358ebc8b50ec4033e7d793509a
cohyunoh <coh00@stuy.edu>
2019-03-27 10:23:18 -0400

    coded second version of eval using Scanner

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Calculator{
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
@@ -6,17 +7,15 @@ public class Calculator{
       MyDeque<String> dec = new MyDeque<String>();
       String operators = "-+*/%";
       double ans = 0.0;
-      for(int i = 0; i < s.length(); i++){
-        if(!s.substring(i, i+1).equals(" ")){
-          String inpt = s.substring(i, i+1);
-          if(operators.contains(s.substring(i, i+1))){
-            double comb = operate((double)Integer.parseInt(dec.removeFirst()), (double)Integer.parseInt(dec.removeFirst()), s.substring(i, i+1));
-            ans += comb;
-            inpt = (int)comb + " ";
-          }
-          dec.addFirst(inpt);
+      Scanner string = new Scanner(s);
+      while(string.hasNext()){
+        String inpt = string.next();
+        if(operators.contains(inpt)){
+          double comb = operate((double)Integer.parseInt(dec.removeFirst()), (double)Integer.parseInt(dec.removeFirst()), inpt);
+          ans += comb;
+          inpt = (int)comb + " ";
         }
-
+        dec.addFirst(inpt);
       }
 
       return ans;
@@ -34,9 +33,10 @@ public class Calculator{
       }else if(operator.equals("%")){
         return inpt1 % inpt2;
       }
+      return 0.0;
     }
 
     public static void main(String[] args) {
-      eval("1 2 3 4 5 + * - - ");
+      System.out.println(eval("2 4 71 8.5 + - * 5 + "));
     }
 }
