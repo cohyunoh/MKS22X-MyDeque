2453e49f5304051a28dfa7c8d719a1acb1a64e4c
cohyunoh <coh00@stuy.edu>
2019-03-27 10:19:27 -0400

    made an operator method to combine values

--- Calculator.java
+++ Calculator.java
@@ -4,14 +4,36 @@ public class Calculator{
      */
     public static double eval(String s){
       MyDeque<String> dec = new MyDeque<String>();
+      String operators = "-+*/%";
+      double ans = 0.0;
       for(int i = 0; i < s.length(); i++){
         if(!s.substring(i, i+1).equals(" ")){
-          dec.addLast(s.substring(i, i+1));
+          String inpt = s.substring(i, i+1);
+          if(operators.contains(s.substring(i, i+1))){
+            double comb = operate((double)Integer.parseInt(dec.removeFirst()), (double)Integer.parseInt(dec.removeFirst()), s.substring(i, i+1));
+            ans += comb;
+            inpt = (int)comb + " ";
+          }
+          dec.addFirst(inpt);
         }
-        //System.out.println(dec);
+
       }
 
-      return 0.0;
+      return ans;
+    }
+
+    private static double operate(double inpt1, double inpt2, String operator){
+      if(operator.equals("+")){
+        return inpt1 + inpt2;
+      }else if(operator.equals("-")){
+        return inpt1 - inpt2;
+      }else if(operator.equals("*")){
+        return inpt1 * inpt2;
+      }else if(operator.equals("/")){
+        return inpt1 / inpt2;
+      }else if(operator.equals("%")){
+        return inpt1 % inpt2;
+      }
     }
 
     public static void main(String[] args) {
